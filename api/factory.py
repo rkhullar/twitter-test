db5511da40b9dbb14185be5191e3cde472d2c397
Rajan Khullar <rkhullar03@gmail.com>
2023-07-23 21:49:56 -0400

    save

--- api/factory.py
+++ api/factory.py
@@ -3,17 +3,20 @@ from fastapi import FastAPI
 from .config import Settings
 from .router import router as api_router
 
+from starlette.middleware.sessions import SessionMiddleware
+
 
 def create_app(settings: Settings) -> FastAPI:
     app = FastAPI(
         settings=settings,
         # swagger_ui_oauth2_redirect_url='/test',
-        swagger_ui_init_oauth={
-            'clientId': settings.twitter_client_id,
-            'clientSecret': settings.twitter_client_secret,
-            'usePkceWithAuthorizationCodeGrant': True,
-            'scopes': ' '.join(settings.twitter_scopes)
-        }
+        # swagger_ui_init_oauth={
+        #     'clientId': settings.twitter_client_id,
+        #     # 'clientSecret': settings.twitter_client_secret,
+        #     'usePkceWithAuthorizationCodeGrant': True,
+        #     'scopes': ' '.join(settings.twitter_scopes)
+        # }
     )
+    app.add_middleware(SessionMiddleware, secret_key='some-random-string')
     app.include_router(api_router)
     return app
--- api/router.py
+++ api/router.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Security, Request
-from fastapi.security import OAuth2AuthorizationCodeBearer
+# from fastapi.security import OAuth2AuthorizationCodeBearer
+from authlib.integrations.starlette_client import OAuth
+from starlette.config import Config
 from .config import Settings
 
 router = APIRouter()
 settings = Settings()
 
 
+'''
 class TwitterOAuth2(OAuth2AuthorizationCodeBearer):
     auth_endpoint: str = 'https://twitter.com/i/oauth2/authorize'
     token_endpoint: str = 'https://api.twitter.com/2/oauth2/token'
@@ -16,17 +19,14 @@ class TwitterOAuth2(OAuth2AuthorizationCodeBearer):
             tokenUrl=self.token_endpoint,
             scopes={scope: scope for scope in scopes}
         )
+'''
 
+# auth_scheme = TwitterOAuth2(scopes=settings.twitter_scopes)
 
-auth_scheme = TwitterOAuth2(scopes=settings.twitter_scopes)
-
-
-@router.get('/test', include_in_schema=True)
-async def test_redirect(request: Request):
-    print(request)
-    return str(request)
+config = Config()
+oauth = OAuth()
 
 
 @router.get('/hello-world')
-async def hello_world(token: str = Security(auth_scheme)):
-    return dict(message='hello world', token=token)
+async def hello_world():
+    return dict(message='hello world')
